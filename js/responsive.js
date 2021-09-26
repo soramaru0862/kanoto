15ddff0af79ff6b256ed50f51391c43cb101f4f8
Hiroki Tatara <tatara0862@gmail.com>
2021-09-27 05:20:09 +0900

    レスポンシブ対応

---
+++ js/responsive.js
@@ -0,0 +1,16 @@
+'use strict'
+// 360px以下のレスポンシブ
+window.addEventListener('DOMContentLoaded', () => {
+  const viewport = document.querySelector('meta[name="viewport"]')
+  function switchViewport () {
+    const value =
+      window.outerWidth > 360
+        ? 'width=device-width,initial-scale=1'
+        : 'width=360'
+    if (viewport.getAttribute('content') !== value) {
+      viewport.setAttribute('content', value)
+    }
+  }
+  addEventListener('resize', switchViewport, false)
+  switchViewport()
+})
